de73e9ece27db7c0bcd119550ed7cac53ad12a32
lenovo <nazarenko.user@gmail.com>
2023-11-26 01:35:33 +0200

    font changed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=noto-sans:300" rel="stylesheet" />
 
         <!-- Scripts -->
         @routes
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,8 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                // sans: ['"Figtree"', ...defaultTheme.fontFamily.sans],
+                sans: ['"Noto Sans"', ...defaultTheme.fontFamily.sans],
             },
         },
     },
