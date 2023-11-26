fbca384b0c337e20ea95be5eb398fbebc719d14a
lenovo <nazarenko.user@gmail.com>
2023-11-26 05:02:27 +0200

    profile buttons rework

--- resources/js/Layouts/NavbarLayout.vue
+++ resources/js/Layouts/NavbarLayout.vue
@@ -112,21 +112,19 @@ export default {
                         <!--Profile-->
                         <Link :href="route('/')">
                             <SecondaryButton class="h-9 ml-3">
-                                <span class="mr-1">Профіль |</span>
-                                <svg class="w-5 h-5 text-gray-800 ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                <svg class="w-5 h-5 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                      fill="none" viewBox="0 0 20 16">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.656 12.115a3 3 0 0 1 5.682-.015M13 5h3m-3 3h3m-3 3h3M2 1h16a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1Zm6.5 4.5a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M3.656 12.115a3 3 0 0 1 5.682-.015M13 5h3m-3 3h3m-3 3h3M2 1h16a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1Zm6.5 4.5a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
                                 </svg>
                             </SecondaryButton>
                         </Link>
                         <!--Cart-->
                         <Link :href="route('/')">
                             <SecondaryButton class="h-9 ml-3">
-                                <span class="mr-1">Корзина |</span>
-                                <svg class="w-5 h-5 text-gray-800 ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                <svg class="w-5 h-5 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                      fill="none" viewBox="0 0 18 20">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                          stroke-width="2"
+                                          stroke-width="1"
                                           d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1"/>
                                 </svg>
                             </SecondaryButton>
@@ -134,11 +132,10 @@ export default {
                         <!--Logout-->
                         <Link :href="route('logout')" method="post" as="button">
                             <SecondaryButton class="h-9 ml-3">
-                                <span class="mr-1">Вихід |</span>
                                 <svg class="w-5 h-5 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                      fill="none" viewBox="0 0 18 15">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                          stroke-width="2"
+                                          stroke-width="1"
                                           d="M1 7.5h11m0 0L8 3.786M12 7.5l-4 3.714M12 1h3c.53 0 1.04.196 1.414.544.375.348.586.82.586 1.313v9.286c0 .492-.21.965-.586 1.313A2.081 2.081 0 0 1 15 14h-3"/>
                                 </svg>
                             </SecondaryButton>
